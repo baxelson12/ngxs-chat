3208ddaf319e3df1ea479562ba8c82967df7c7af
baxelson12 <bradaxelson@gmail.com>
2020-02-17 18:54:04 -0500

    Build component layout and tweak design

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -4,21 +4,28 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MessageComponent } from './message/message.component';
+import { MessageBarComponent } from './message-bar/message-bar.component';
 import { UserComponent } from './user/user.component';
 import { MessageContainerComponent } from './message-container/message-container.component';
+import { UserContainerComponent } from './user-container/user-container.component';
+import { NgxsModule } from '@ngxs/store';
+import { AppState } from './state/app.state';
 
 @NgModule({
   declarations: [
     AppComponent,
     MessageComponent,
     UserComponent,
-    MessageContainerComponent
+    MessageContainerComponent,
+    UserContainerComponent,
+    MessageBarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgxsModule.forRoot([AppState])
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
